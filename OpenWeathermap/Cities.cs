4f3330f83d7b1fff177be46febbb8fefb05c21f7
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-07-30 07:53:15 +0200

    CitiesTest.TestGetGermanCities works. Backend returns hard-coded city. First step

---
+++ OpenWeathermap/Cities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend.OpenWeathermap
+{
+    public static class Cities
+    {
+        private static Lazy<IEnumerable<City>> cities = 
+            new Lazy<IEnumerable<City>>(() => ReadCitiesFromJSON());
+        public static IEnumerable<City> All => cities.Value;
+
+        private static IEnumerable<City> ReadCitiesFromJSON()
+        {
+            yield return new City { Id = 42, Country = "DE" };
+        }
+    }
+}
---
+++ OpenWeathermap/City.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+namespace Backend.OpenWeathermap
+{
+    /// <summary>
+    /// City returned from city.list.json
+    /// </summary>
+    public class City
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "state")]
+        public string State { get; set; }
+
+        [JsonProperty(PropertyName = "country")]
+        public string Country { get; set; }
+
+        [JsonProperty(PropertyName = "coord")]
+        public Coordinate Coordinate { get; set; }
+    }
+}
+
