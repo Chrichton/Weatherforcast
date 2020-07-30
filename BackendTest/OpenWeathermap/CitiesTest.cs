a14a6d47f7429ccecdec12a4f19f624f22563b84
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-07-30 11:46:44 +0200

    Reorganised FolderStructure

---
+++ BackendTest/OpenWeathermap/CitiesTest.cs
@@ -0,0 +1,17 @@
+﻿using Backend.OpenWeathermap;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace BackendTest.OpenWeathermap
+{
+    public class CitiesTest
+    {
+        [Fact]
+        public void TestGetGermanCities()
+        {
+            IEnumerable<City> cities = Cities.All.Where(city => city.Country == "DE");
+            Assert.True(cities.Count() > 0);
+        }
+    }
+}
---
+++ BackendTest/OpenWeathermap/PlzOrteTest.cs
@@ -0,0 +1,16 @@
+﻿using Backend.OpenWeathermap;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BackendTest.OpenWeathermap
+{
+    public class PlzOrteTest
+    {
+        [Fact]
+        public void TestPlzOrte()
+        {
+            string ort = PlzOrte.Dictionary.GetValueOrDefault("21037", "");
+            Assert.Equal("Hamburg", ort);
+        }
+    }
+}
