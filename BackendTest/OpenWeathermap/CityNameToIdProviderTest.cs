dd51495327aeb81e975c2dcbc120cf8203b324f7
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-08-08 05:03:15 +0200

    ProviderTests

---
+++ BackendTest/OpenWeathermap/CityNameToIdProviderTest.cs
@@ -0,0 +1,32 @@
+﻿using Backend.OpenWeathermap;
+using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BackendTest.OpenWeathermap
+{
+    public class CityNameToIdProviderTest
+    {
+        [Fact]
+        public void TestLiveProvider()
+        {
+            Assert.Equal(2911298, new CityNameToIdProvider().GetDictionary()["Hamburg"]);
+        }
+
+        [Fact]
+        public void TestProvider()
+        {
+            var dict = new Dictionary<string, int> { { "Hamburg", 1} };
+            var provider = new CityNameToIdProvider(dict);
+            Assert.Equal(1, provider.GetDictionary()["Hamburg"]);
+
+            int id;
+            if (provider.GetDictionary().TryGetValue("Hambur", out id))
+            {
+                Assert.False(true, "Test Failed");
+            }
+        }
+    }
+}
---
+++ BackendTest/Weatherforecast/ZipCodeToCitiesProviderTest.cs
@@ -0,0 +1,50 @@
+﻿using Backend.Weatherforecast;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace BackendTest.Weatherforecast
+{
+    public class ZipCodeToCitiesProviderTest
+    {
+        [Fact]
+        public void TestLiveProvider()
+        {
+            IEnumerable<string> result = new ZipCodeToCitiesProvider().GetDictionary()[21037];
+            Assert.Single(new ZipCodeToCitiesProvider().GetDictionary()[21037]);
+            Assert.Equal("Hamburg", result.Single());
+        }
+
+        [Fact]
+        public void TestId()
+        {
+            var dictionary = new Dictionary<int, IEnumerable<string>>
+            {
+                { 1, new [] { "Ort1", "Ort2"} }
+            };
+
+            var provider = new ZipCodeToCitiesProvider(dictionary);
+            IEnumerable<string> result = provider.GetDictionary()[1];
+
+            Assert.Equal(2, result.Count());
+            Assert.Equal("Ort1", result.First());
+        }
+
+        [Fact]
+        public void TestUnknownId()
+        {
+            var dictionary = new Dictionary<int, IEnumerable<string>>
+            {
+                { 1, new [] { "Ort1", "Ort2"} }
+            };
+
+            var provider = new ZipCodeToCitiesProvider(dictionary);
+
+            IEnumerable<string> value;
+            if (provider.GetDictionary().TryGetValue(2, out value))
+            {
+                Assert.False(true, "Test Failed");
+            }      
+        }
+    }
+}
