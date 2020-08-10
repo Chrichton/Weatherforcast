e345b79ade9d96edcbd15586b94c2c3df808bdd7
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-08-10 05:07:58 +0200

    App.vue: removed style CurrentWeather: add datapoints

--- Vue/vue-weather/src/App.vue
+++ Vue/vue-weather/src/App.vue
@@ -4,7 +4,7 @@
       <Header/>
       <TestVuetify v-bind:model="Model.forecast"/>
       <SelectCity v-on:load-weather="loadWeather"/>
-      <CurrentWeather v-bind:model="Model.current"/>
+      <CurrentWeather v-bind:model="Model"/>
       <Forecast v-bind:model="Model.forecast"/>
       <History v-bind:historyData="historyData"/>
     </div>
@@ -161,29 +161,3 @@ export default {
 }
 
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
-
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px, 20px;
-  cursor: pointer;
-}
-
-.btn:hover {
-  background: #666;
-}
-</style>
--- Vue/vue-weather/src/components/CurrentWeather.vue
+++ Vue/vue-weather/src/components/CurrentWeather.vue
@@ -4,10 +4,13 @@
             <h5 class="Display-1">Aktuelles Wetter</h5>
         </v-card-title>
         <v-spacer>
-            {{model.Temperature}} Celsius
-            {{model.MinimumTemperature}} Celsius
-            {{model.MaximumTemperature}} Celsius
-            {{model.Humidity}} %
+            {{model.current.Temperature}} Celsius
+            {{model.current.MinimumTemperature}} Celsius (min)
+            {{model.current.MaximumTemperature}} Celsius (max)
+            {{model.current.Humidity}} %
+            {{model.current.CloudDescription}}
+            {{model.AverageTemperature}} Celsius (avg)
+            {{model.AverageHumidity}} (avg) %
     </v-spacer>
     </v-card>
 </template>
