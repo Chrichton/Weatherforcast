c0cf4ec90855b001642b7c0e82bb19fe23e13367
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-08-10 03:48:11 +0200

    Forecast with v-simple-table. Embedded in v-card

--- Vue/vue-weather/src/components/Forecast.vue
+++ Vue/vue-weather/src/components/Forecast.vue
@@ -1,26 +1,31 @@
 <template>
-  <v-simple-table fixed-header height="300px" dense>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">Datum</th>
-          <th class="text-left">Temperatur</th>
-          <th class="text-left">Temperatur (min)</th>
-          <th class="text-left">Temperatur (max)</th>
-          <th class="text-left">Feuchte</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in forcast" :key="item.DateTime">
-          <td>{{ item.DateTime }} </td>
-          <td>{{ item.Temperature }} Celsius</td>
-          <td>{{ item.MinimumTemperature }} Celsius</td>
-          <td>{{ item.MaximumTemperature }} Celsius</td>
-          <td>{{ item.Humidity }} %</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <v-card>
+      <v-card-title>
+          <h5 class="Display-1">Forecast</h5>
+      </v-card-title>
+      <v-simple-table fixed-header height="300px" dense>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Datum</th>
+              <th class="text-left">Temperatur</th>
+              <th class="text-left">Temperatur (min)</th>
+              <th class="text-left">Temperatur (max)</th>
+              <th class="text-left">Feuchte</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in forcast" :key="item.DateTime">
+              <td>{{ item.DateTime }} </td>
+              <td>{{ item.Temperature }} Celsius</td>
+              <td>{{ item.MinimumTemperature }} Celsius</td>
+              <td>{{ item.MaximumTemperature }} Celsius</td>
+              <td>{{ item.Humidity }} %</td>
+            </tr>
+          </tbody>    
+        </template>
+      </v-simple-table>
+  </v-card>
 </template>
 
 <script>
--- Vue/vue-weather/src/components/History.vue
+++ Vue/vue-weather/src/components/History.vue
@@ -1,24 +1,36 @@
 <template>
-    <div> 
-        <h3>History</h3>
-        <div v-for="historyItem in historyData" :key="historyItem.id">
-            <HistoryItem v-bind:historyItem="historyItem"/>
-        </div>
-    </div>    
+<v-card>
+    <v-card-title>
+        <h5 class="Display-1">History</h5>
+    </v-card-title>
+    <v-simple-table fixed-header height="300px" dense>
+        <template v-slot:default>
+        <thead>
+            <tr>
+            <th class="text-left">Ort</th>
+            <th class="text-left">Temperatur</th>
+            <th class="text-left">Feuchte</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in data" :key="item.City">
+            <td>{{ item.city }} </td>
+            <td>{{ item.temperature }} Celsius</td>
+            <td>{{ item.humidity }} %</td>
+            </tr>
+        </tbody>
+        </template>
+    </v-simple-table>
+</v-card>
 </template>
 
 <script>
-import HistoryItem from './HistoryItem';
-
-export default {
-   name: "History",
-   components: {
-       HistoryItem
-   },
-   props: ["historyData"]
-}
+  export default {
+    props: ["historyData"],
+    data () {
+      return {
+        data: this.historyData
+      }
+    },
+  }
 </script>
-
-<style scoped>
-
-</style>
